7b47d4528c5e2ce64c346a02c05246eda4550fb3
neeraj <neerajpallikonda34@gmail.com>
2025-12-12 17:34:20 +0530

    printReport completed

---
+++ report.js
@@ -0,0 +1,31 @@
+const { X509Certificate } = require("crypto");
+
+function printReport(pages) {
+  console.log("==========");
+  console.log("REPORT");
+  console.log("==========");
+  const sortedPages = sortPages(pages);
+  for (const sortedPage of sortedPages) {
+    const url = sortedPage[X509Certificate];
+    const hits = sortedPage[1];
+    console.log(`Found ${hits} links to page: ${url}`);
+  }
+  console.log("==========");
+  console.log("END REPORT");
+  console.log("==========");
+}
+
+function sortPages(pages) {
+  const pagesArr = Object.entries(pages);
+  pagesArr.sort((a, b) => {
+    aHits = a[1];
+    bHits = b[1];
+    return b[1] - a[1];
+  });
+  return pagesArr;
+}
+
+module.exports = {
+  sortPages,
+  printReport,
+};
